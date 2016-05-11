f909f95d952110953d772474f2d4750b0e3c339f
Juan Sebastian Vargas <juan_vargas10@hotmail.com>
2016-05-11 16:13:08 -0500

    down

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "inicial.h"
+#include "evolve.h"
 /*
   Un programa sencillo para seguir la evolucion
   dinamica de un conjunto de N masas puntuales.
@@ -10,28 +12,42 @@ int main(int argc, char **argv){
   double *p=NULL;
   double *v=NULL;
   double *a=NULL;
+  double *U=NULL;
+  double *K=NULL;
   double time_step=0, total_time=0, epsilon=0;
   int n_steps=0;
 
   /*inicializacion*/
   recibe_input(argc, argv, &N, &epsilon);
-  p = crea_vector(N);
-  v = crea_vector(N);
-  a = crea_vector(N);
+  p = crea_vector(3*N);
+  v = crea_vector(3*N);
+  a = crea_vector(3*N);
+  U = crea_vector(N);
+  K = crea_vector(N);
   posiciones_iniciales(p, N);
-  escribe_estado(p,v,i);
+  calcula_energia(p, v, U, K, N);
+  escribe_estado(p, v, U, K, N, i);
 
-  /*tiempos caracteristicos*/
-  total_time = calcula_tiempo_total(N);
-  time_step = calcula_time_step(N);
-  n_steps = (int)(total_time/time_step);
   
-  /*evolucion temporal*/
+  // tiempos caracteristicos 
+  total_time = calcula_tiempo_total(N);
+  time_step = calcula_time_step(N, epsilon);
+  n_steps = (int)(total_time/time_step);
+  fprintf(stderr, "tiempo total: %f time_step: %f n_steps %d\n", 
+	  total_time, time_step, n_steps);
+  //evolucion temporal
+
+  calcula_aceleracion(p, v, a, N, epsilon);
+  kick(p, v, a, N, time_step/2.0);  
   for(i=0;i<n_steps;i++){
+    drift(p, v, a, N, time_step);  
     calcula_aceleracion(p, v, a, N, epsilon);
-    actualiza_timestep(p, v, a, N);
+    kick(p, v, a, N, time_step);  
+
   }
-  escribe_estado(p,v,i)
+  calcula_energia(p, v, U, K, N);
+  escribe_estado(p, v, U, K, N, i);    
+  
   return 0;
 }
 
